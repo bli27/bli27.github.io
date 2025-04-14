2d1d01e711b32af595bb7c09f58a12b811a4dc4c
bli27 <li.brya@northeastern.edu>
2025-04-14 17:54:35 -0400

    about me edit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ function About() {
         <img src={aboutMeImage} alt="About Me" className="about-me-image" />
         <p className="about-me-text">
           Hello! My name is Bryan and I am currently on my first co-op as a Software Engineer at Barrett Distribution Centers!
-          I am passionate about all facets of technology and utilizing tools to create platforms that can help benefit my community. 
+          I am passionate about all areas of technology and utilizing tools to create platforms that can help benefit my community. 
           Throughout my time as a student, I’ve developed a strong understanding of algorithms, data structures, software development, and object-oriented programming.
           In my free time, I love to play basketball, hang out with friends/family, travel around the city and explore new places to eat!
         </p>
